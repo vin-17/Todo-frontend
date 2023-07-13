fe908e6c059dbe1d9fa2d0f787f358c471d3ca64
Vineet Singh <vineet.yo3@gmail.com>
2023-07-14 02:03:03 +0530

    minor

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -27,8 +27,8 @@ const styles = {
   container: {
     display: "flex",
     flexDirection: "column",
+    alignItems: "center",
     
-    justifyContent: "center",
     height: "100vh",
   },
   gap: {
